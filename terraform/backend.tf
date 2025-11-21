7a7a307ce5fd551e27cb28bde6612f1854c5be2b
Rodrigo Azevedo <zvdrodrigo@gmail.com>
2025-11-21 10:49:48 -0300

    Gitlab reactive

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,4 @@
 terraform {
-  backend "local" {
-    # Caminho ABSOLUTO na sua workstation
-    # Ajuste '/home/rodrigo' se seu usuário for diferente
-    path = "/home/rodrigo/terraform-states/k3s-homelab.tfstate"
+  backend "http" {
   }
 }
